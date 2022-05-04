aec1c87ca525079e5085949995a88dd2b9a157b9
Jakub Krišák <102148634+JakubKrisak@users.noreply.github.com>
2022-05-04 20:15:45 +0200

    Add files via upload

---
+++ myScript1.js
@@ -0,0 +1,28 @@
+function example1() {
+
+    let x, y, a, b, c, d;
+    x = 4;
+    y = 2;
+    a = x + y;
+    b = x - y;
+    c = x * y;
+    d = x / y;
+
+    console.log("4");
+    console.log("2");
+
+    console.log("Vykonávam matematické operácie");
+
+    console.log("4 + 2 = " + a);
+    console.log("4 - 2 = " + b);
+    console.log("4 * 2 = " + c);
+    console.log("4 / 2 = " + d);
+}
+
+function example2() {
+
+    let number1, number2;
+    number1 = 4;
+    number2 = 2;
+    
+}
